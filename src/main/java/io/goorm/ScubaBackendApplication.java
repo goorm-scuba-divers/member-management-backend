8da228f657ce49c07c3f76e5a2521e2d49a00fb9
theo-ooooo <kkw.theo@gmail.com>
2025-05-14 14:48:11 +0900

    feature: project setting

---
+++ src/main/java/io/goorm/ScubaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.goorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScubaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScubaBackendApplication.class, args);
+    }
+
+}
