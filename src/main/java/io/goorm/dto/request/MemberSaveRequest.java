55df784ad4ebbf2dc5e3520c4e9f1937f53ef972
eunlee <eklee2057@naver.com>
2025-05-19 23:03:35 +0900

    fix: update validation message for username and add Swagger annotations for MemberSaveRequest

--- src/main/java/io/goorm/dto/request/MemberSaveRequest.java
+++ src/main/java/io/goorm/dto/request/MemberSaveRequest.java
@@ -1,12 +1,18 @@
 package io.goorm.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record MemberSaveRequest(
-        @NotNull(message = "username null이면 안됌")
+        @NotNull(message = "username null이면 안 됨")
+        @Schema(description = "아이디", example = "goorm")
         String username,
+
         @NotNull
+        @Schema(description = "닉네임", example = "scuba")
         String nickname,
+
         @NotNull
+        @Schema(description = "비밀번호", example = "1234")
         String password) {
 }
