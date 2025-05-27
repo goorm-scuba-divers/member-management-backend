57187cdd9c8b8e7988d028cc2c176202385ad9d6
eunlee <eklee2057@naver.com>
2025-05-27 15:07:50 +0900

    refactor: update MemberController to improve method signatures and remove unused code

--- src/main/java/io/goorm/member/controller/MemberController.java
+++ src/main/java/io/goorm/member/controller/MemberController.java
@@ -1,39 +1,59 @@
 package io.goorm.member.controller;
 
-import io.goorm.member.dto.request.MemberSaveRequest;
+import io.goorm.config.dto.PrincipalDetails;
+import io.goorm.member.dto.request.MemberUpdateRequest;
 import io.goorm.member.dto.response.MemberFindMeResponse;
+import io.goorm.member.dto.response.MemberResponse;
 import io.goorm.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
+@RequiredArgsConstructor
 @Tag(name = "회원 API", description = "회원 관련 API")
 public class MemberController {
     private final MemberService memberService;
 
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     // 리스트 조회
-//    @GetMapping()
-
-    // 단수 조회
-//    @GetMapping("/{id}")
+    @GetMapping()
+    @Operation(summary = "회원 리스트 조회", description = "회원 리스트 조회 API")
+    public List<MemberResponse> findAll(
+            @PageableDefault(size = 10, direction = Sort.Direction.DESC, sort = "createdAt", page = 0) Pageable pageable,
+            @RequestParam(required = false) String nickname
+            ) {
+        return memberService.findAll();
+    }
 
     // 내 프로필 조회
     @GetMapping("/me")
     @Operation(summary = "내 프로필 조회", description = "내 프로필 조회 API")
-    public MemberFindMeResponse findMember() {
-        return memberService.findMember();
+    public MemberFindMeResponse findMember(@AuthenticationPrincipal PrincipalDetails userDetails) {
+        return memberService.findMember(userDetails);
     }
 
     // 수정
-//    @PutMapping
+    @PutMapping
+    @Operation(summary = "내 프로필 수정", description = "내 프로필 수정 API")
+    public void updateMember(@AuthenticationPrincipal PrincipalDetails userDetails,
+                             @Valid @RequestBody MemberUpdateRequest request) {
+        memberService.updateMember(userDetails, request);
+    }
 
     // 삭제
-//    @DeleteMapping()
+    @DeleteMapping
+    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴 API")
+    public void deleteMember(@AuthenticationPrincipal PrincipalDetails userDetails) {
+        memberService.deleteMember(userDetails);
+    }
 }
