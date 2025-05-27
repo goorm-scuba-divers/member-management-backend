23ecd5ada2ad65658f95d1625c71c4083a026c5f
eunlee <eklee2057@naver.com>
2025-05-27 15:10:11 +0900

    refactor: update AuthController to streamline member registration and enhance refresh token handling

--- src/main/java/io/goorm/auth/controller/AuthController.java
+++ src/main/java/io/goorm/auth/controller/AuthController.java
@@ -1,31 +1,33 @@
 package io.goorm.auth.controller;
 
+import io.goorm.auth.dto.request.AuthRefreshTokenReIssueRequest;
 import io.goorm.auth.dto.request.AuthSignInRequest;
+import io.goorm.auth.dto.request.AuthSignOutRequest;
 import io.goorm.auth.dto.response.AuthSignInResponse;
 import io.goorm.auth.service.AuthService;
 import io.goorm.member.dto.request.MemberSaveRequest;
 import io.goorm.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@Tag(name = "인증 API", description = "인증 관련 API")
 public class AuthController {
 
-    private final MemberService memberService;
     private final AuthService authService;
-
-    public AuthController(MemberService memberService, AuthService authService) {
-        this.memberService = memberService;
-        this.authService = authService;
-    }
 
     @PostMapping("/auth/sign-up")
     @Operation(summary = "회원가입", description = "회원가입 API")
-    public void signUp(@Valid @RequestBody MemberSaveRequest request) {
-        memberService.save(request);
+    public AuthSignInResponse signUp(@Valid @RequestBody MemberSaveRequest request) {
+        return authService.save(request);
     }
 
     @PostMapping("/auth/sign-in")
@@ -33,4 +35,21 @@ public class AuthController {
     public AuthSignInResponse signIn(@Valid @RequestBody AuthSignInRequest request) {
         return authService.signIn(request.username(), request.password());
     }
+
+    //리프레쉬 토큰으로 재발급
+    @PostMapping("/auth/reissue")
+    @Operation(summary = "리프레쉬 토큰 재발급", description = "리프레쉬 토큰 재발급 API")
+    @ApiResponse(responseCode = "200", description = "리프레쉬 토큰 재발급 성공")
+    public AuthSignInResponse reissue(@Valid @RequestBody AuthRefreshTokenReIssueRequest request) {
+        return authService.refresh(request.refreshToken());
+    }
+
+    //로그아웃
+    @PostMapping("/auth/sign-out")
+    @Operation(summary = "로그아웃", description = "로그아웃 API")
+    @ApiResponse(responseCode = "200", description = "로그아웃 성공")
+    public void signOut(@Valid @RequestBody AuthSignOutRequest request) {
+        authService.signOut(request.refreshToken());
+    }
+
 }
