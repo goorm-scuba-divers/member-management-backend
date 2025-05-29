72f2e6380c32aab1d68732ff997183979294974b
eunlee <eklee2057@naver.com>
2025-05-29 11:43:01 +0900

    refactor: 개행 수정

--- src/main/java/io/goorm/config/exception/ErrorCode.java
+++ src/main/java/io/goorm/config/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     AUTH_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
     AUTH_USERNAME_PASSWORD_INVALID(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 잘못되었습니다."),
     AUTH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+
     // 멤버
     MEMBER_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
