c8f68052682a689ae47a0bc07f665f6bc803a775
eunlee <eklee2057@naver.com>
2025-05-30 10:44:12 +0900

    feat: 회원 검색을 nickname이 아니라 username에 대하여 수행하도록 수정

--- src/main/java/io/goorm/member/dao/MemberRepository.java
+++ src/main/java/io/goorm/member/dao/MemberRepository.java
@@ -1,8 +1,6 @@
 package io.goorm.member.dao;
 
 import io.goorm.member.domain.Member;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
