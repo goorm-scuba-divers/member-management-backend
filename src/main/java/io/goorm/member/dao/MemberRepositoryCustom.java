300736657b8957b9757e3ad69551b7bc0af1b758
eunlee <eklee2057@naver.com>
2025-05-30 11:25:28 +0900

    feat: 검색 조건에 id 추가

--- src/main/java/io/goorm/member/dao/MemberRepositoryCustom.java
+++ src/main/java/io/goorm/member/dao/MemberRepositoryCustom.java
@@ -1,9 +1,10 @@
 package io.goorm.member.dao;
 
 import io.goorm.member.domain.Member;
+import io.goorm.member.domain.MemberRole;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MemberRepositoryCustom {
-    Page<Member> findAllByPageableAndFilter(Pageable pageable, String username);
+    Page<Member> findAllByPageableAndFilter(Pageable pageable, String searchValue, MemberRole role);
 }
