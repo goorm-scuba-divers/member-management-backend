39a985fa32f80ee7f5d02ab985042e4457e38e9d
eunlee <eklee2057@naver.com>
2025-06-03 18:28:53 +0900

    fix: 회원가입 또는 로그인의 결과로 보내주는 token 외에도 사용자 본인의 정보를 보내주도록 수정

--- src/main/java/io/goorm/auth/dto/response/AuthSignInResponse.java
+++ src/main/java/io/goorm/auth/dto/response/AuthSignInResponse.java
@@ -1,5 +1,6 @@
 package io.goorm.auth.dto.response;
 
+import io.goorm.member.domain.MemberRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record AuthSignInResponse (
@@ -7,9 +8,21 @@ public record AuthSignInResponse (
         String accessToken,
 
         @Schema(description = "리프레시 토큰")
-        String refreshToken
+        String refreshToken,
+
+        @Schema(description = "고유 아이디")
+        Long id,
+
+        @Schema(description = "아이디", example = "goorm")
+        String username,
+
+        @Schema(description = "닉네임", example = "scuba")
+        String nickname,
+
+        @Schema(description = "권한")
+        MemberRole role
 ) {
-    public static AuthSignInResponse of(String accessToken, String refreshToken) {
-        return new AuthSignInResponse(accessToken, refreshToken);
+    public static AuthSignInResponse of(String accessToken, String refreshToken, Long id, String username, String nickname, MemberRole role) {
+        return new AuthSignInResponse(accessToken, refreshToken, id, username, nickname, role);
     }
 }
