0e2f94620003506ea2f850796a13587c855975f5
eunlee <eklee2057@naver.com>
2025-06-03 19:27:45 +0900

    feat: 로그인 시 토큰 객체와 멤버 정보 객체를 반환하도록 DTO 추가

---
+++ src/main/java/io/goorm/auth/dto/response/AuthMemberInfo.java
@@ -0,0 +1,22 @@
+package io.goorm.auth.dto.response;
+
+import io.goorm.member.domain.MemberRole;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AuthMemberInfo (
+        @Schema(description = "고유 아이디")
+        Long id,
+
+        @Schema(description = "아이디", example = "goorm")
+        String username,
+
+        @Schema(description = "닉네임", example = "scuba")
+        String nickname,
+
+        @Schema(description = "권한")
+        MemberRole role
+) {
+    public static AuthMemberInfo from(Long id, String username, String nickname, MemberRole role) {
+        return new AuthMemberInfo(id, username, nickname, role);
+    }
+}
--- src/main/java/io/goorm/auth/dto/response/AuthSignInResponse.java
+++ src/main/java/io/goorm/auth/dto/response/AuthSignInResponse.java
@@ -1,28 +1,15 @@
 package io.goorm.auth.dto.response;
 
-import io.goorm.member.domain.MemberRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record AuthSignInResponse (
-        @Schema(description = "액세스 토큰")
-        String accessToken,
+        @Schema(description = "토큰 정보")
+        AuthTokenInfo tokens,
 
-        @Schema(description = "리프레시 토큰")
-        String refreshToken,
-
-        @Schema(description = "고유 아이디")
-        Long id,
-
-        @Schema(description = "아이디", example = "goorm")
-        String username,
-
-        @Schema(description = "닉네임", example = "scuba")
-        String nickname,
-
-        @Schema(description = "권한")
-        MemberRole role
+        @Schema(description = "회원 정보")
+        AuthMemberInfo member
 ) {
-    public static AuthSignInResponse of(String accessToken, String refreshToken, Long id, String username, String nickname, MemberRole role) {
-        return new AuthSignInResponse(accessToken, refreshToken, id, username, nickname, role);
+    public static AuthSignInResponse of(AuthTokenInfo tokenInfo, AuthMemberInfo memberInfo) {
+        return new AuthSignInResponse(tokenInfo, memberInfo);
     }
 }
