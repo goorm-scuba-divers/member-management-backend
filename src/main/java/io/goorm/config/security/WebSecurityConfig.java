d639f36d167c2892c2e16279bb407aec50f3ef82
eunlee <eklee2057@naver.com>
2025-06-04 01:31:21 +0900

    chore: /members 리스트 조회에 대한 권한을 추후 ADMIN에 한해서만 수정해야 함을 주석으로 표시

--- src/main/java/io/goorm/config/security/WebSecurityConfig.java
+++ src/main/java/io/goorm/config/security/WebSecurityConfig.java
@@ -2,14 +2,13 @@ package io.goorm.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -41,10 +40,10 @@ public class WebSecurityConfig {
                 authorize ->
                         authorize
                                 .requestMatchers("/auth/**").permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/members").hasRole("ADMIN")
                                 .requestMatchers("/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
         );
-
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
