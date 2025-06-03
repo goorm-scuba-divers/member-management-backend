b0c92c80bfa1045f4a46fda4ec836f43e1d6d281
eunlee <eklee2057@naver.com>
2025-06-03 18:00:06 +0900

    fix: 내 프로필 조회 결과에 role 추가

--- src/main/java/io/goorm/member/dto/response/MemberFindMeResponse.java
+++ src/main/java/io/goorm/member/dto/response/MemberFindMeResponse.java
@@ -1,14 +1,18 @@
 package io.goorm.member.dto.response;
 
 import io.goorm.member.domain.Member;
+import io.goorm.member.domain.MemberRole;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MemberFindMeResponse (
         @Schema(description = "닉네임", example = "scuba")
-        String nickname
+        String nickname,
+
+        @Schema(description = "권한")
+        MemberRole role
 ) {
 
     public static MemberFindMeResponse from(Member member) {
-        return new MemberFindMeResponse(member.getNickname());
+        return new MemberFindMeResponse(member.getNickname(), member.getRole());
     }
 }
