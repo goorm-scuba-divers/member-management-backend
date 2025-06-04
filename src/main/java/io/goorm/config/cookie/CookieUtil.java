0bb8e1d8ec00d33a12afd0ddf2b7a9a5b8c5f4ed
theo-ooooo <kkw.theo@gmail.com>
2025-06-04 11:15:25 +0900

    feature: cookie util add

---
+++ src/main/java/io/goorm/config/cookie/CookieUtil.java
@@ -0,0 +1,73 @@
+package io.goorm.config.cookie;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.server.Cookie;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class CookieUtil {
+
+    private static final String ACCESS_TOKEN_COOKIE_NAME = "access_token";
+    private static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+
+    public HttpHeaders generateTokenCookies(String accessToken, String refreshToken) {
+
+        String sameSite = getSamSitePolicy();
+
+        ResponseCookie accessTokenCookie = ResponseCookie.from(ACCESS_TOKEN_COOKIE_NAME, accessToken)
+                .path("/")
+                .secure(true)
+                .httpOnly(true)
+                .sameSite(sameSite)
+                .build();
+
+        ResponseCookie refreshTokenCookie = ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, refreshToken)
+                .path("/")
+                .secure(true)
+                .httpOnly(true)
+                .sameSite(sameSite)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
+        headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+        return headers;
+    }
+
+    private static String getSamSitePolicy() {
+        // 쿠키 전송 방식.
+        return Cookie.SameSite.NONE.attributeValue();
+    }
+
+    public HttpHeaders deleteTokenCookies() {
+       String sameSite = getSamSitePolicy();
+
+        ResponseCookie accessTokenCookie = ResponseCookie.from(ACCESS_TOKEN_COOKIE_NAME, "")
+                .path("/")
+                .secure(true)
+                .httpOnly(true)
+                .sameSite(sameSite)
+                .maxAge(0)
+                .build();
+
+        ResponseCookie refreshTokenCookie = ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, "")
+                .path("/")
+                .secure(true)
+                .httpOnly(true)
+                .sameSite(sameSite)
+                .maxAge(0)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
+        headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+        return headers;
+    }
+}
