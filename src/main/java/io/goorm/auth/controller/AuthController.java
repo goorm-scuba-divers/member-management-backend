fefc0c88d2489c26ddc75f39fa80e35205eaf4be
theo-ooooo <kkw.theo@gmail.com>
2025-06-04 11:19:22 +0900

    feature: auth controller cookie add

--- src/main/java/io/goorm/auth/controller/AuthController.java
+++ src/main/java/io/goorm/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import io.goorm.auth.dto.request.AuthSignOutRequest;
 import io.goorm.auth.dto.response.AuthSignInResponse;
 import io.goorm.auth.dto.response.AuthTokenResponse;
 import io.goorm.auth.service.AuthService;
+import io.goorm.config.cookie.CookieUtil;
 import io.goorm.member.dto.request.MemberSaveRequest;
 import io.goorm.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,6 +15,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,17 +28,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final CookieUtil cookieUtil;
 
     @PostMapping("/auth/sign-up")
     @Operation(summary = "회원가입", description = "회원가입 API")
-    public AuthSignInResponse signUp(@Valid @RequestBody MemberSaveRequest request) {
-        return authService.save(request);
+    public ResponseEntity<AuthSignInResponse> signUp(@Valid @RequestBody MemberSaveRequest request) {
+
+        AuthSignInResponse response = authService.save(request);
+
+        String accessToken = response.tokens().accessToken();
+        String refreshToken = response.tokens().refreshToken();
+
+        HttpHeaders tokenHeaders = cookieUtil.generateTokenCookies(accessToken, refreshToken);
+
+        return ResponseEntity.status(HttpStatus.CREATED).headers(tokenHeaders).body(response);
     }
 
     @PostMapping("/auth/sign-in")
     @Operation(summary = "로그인", description = "로그인 API")
-    public AuthSignInResponse signIn(@Valid @RequestBody AuthSignInRequest request) {
-        return authService.signIn(request.username(), request.password());
+    public ResponseEntity<AuthSignInResponse> signIn(@Valid @RequestBody AuthSignInRequest request) {
+        AuthSignInResponse response = authService.signIn(request.username(), request.password());
+        String accessToken = response.tokens().accessToken();
+        String refreshToken = response.tokens().refreshToken();
+
+        HttpHeaders tokenHeaders = cookieUtil.generateTokenCookies(accessToken, refreshToken);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(tokenHeaders).body(response);
     }
 
     //리프레쉬 토큰으로 재발급
@@ -50,6 +69,7 @@ public class AuthController {
     @Operation(summary = "로그아웃", description = "로그아웃 API")
     @ApiResponse(responseCode = "200", description = "로그아웃 성공")
     public void signOut(@Valid @RequestBody AuthSignOutRequest request) {
+        cookieUtil.deleteTokenCookies();
         authService.signOut(request.refreshToken());
     }
 
