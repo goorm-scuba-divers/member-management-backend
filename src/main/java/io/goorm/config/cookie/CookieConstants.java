09c254375b2a88a92c8ce2d37f891b3bbacb717d
eunlee <eklee2057@naver.com>
2025-06-04 16:23:47 +0900

    feat: 액세스 토큰 및 Cookie를 기반으로 refresh 수행

---
+++ src/main/java/io/goorm/config/cookie/CookieConstants.java
@@ -0,0 +1,8 @@
+package io.goorm.config.cookie;
+
+public final class CookieConstants {
+    public static final String ACCESS_TOKEN_COOKIE_NAME = "access_token";
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+
+    private CookieConstants() {}
+}
--- src/main/java/io/goorm/config/security/JwtAuthenticationFilter.java
+++ src/main/java/io/goorm/config/security/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package io.goorm.config.security;
 
+import io.goorm.auth.dto.response.AuthTokenResponse;
+import io.goorm.auth.service.AuthService;
+import io.goorm.config.cookie.CookieUtil;
 import io.goorm.config.dto.PrincipalDetails;
 import io.goorm.config.dto.TokenDto;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,40 +18,83 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import static io.goorm.config.cookie.CookieConstants.*;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
-
-    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+    private final CookieUtil cookieUtil;
+    private final AuthService authService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // JWT 토큰을 요청 헤더에서 가져오기
-        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
-        // JWT 토큰 검증 및 사용자 정보 설정
-        if (hasBearerToken(token)) {
-            TokenDto tokenDto = jwtUtil.parseToken(getToken(token));
+        String accessTokenByHeader = getAccessTokenByHeader(request);
+        String accessTokenByCookie = getAccessTokenByCookie(request);
+        String refreshTokenByCookie = getRefreshTokenByCookie(request);
 
-            if (tokenDto != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = new PrincipalDetails(tokenDto.memberId(), tokenDto.memberRole());
-                Authentication authentication = UsernamePasswordAuthenticationToken.authenticated(userDetails, null, userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
+        String accessToken = accessTokenByHeader != null ? accessTokenByHeader : accessTokenByCookie;
+
+        // 액세스 토큰이 존재할때.
+        if (accessToken != null) {
+            setAuthentication(accessTokenByHeader);
+            filterChain.doFilter(request, response);
+
+            return;
         }
 
+        if (refreshTokenByCookie != null) {
+            AuthTokenResponse authTokenResponse = authService.refresh(refreshTokenByCookie);
+
+            HttpHeaders httpHeaders = cookieUtil.generateTokenCookies(authTokenResponse.accessToken(), authTokenResponse.refreshToken());
+
+            Objects.requireNonNull(httpHeaders.get(HttpHeaders.SET_COOKIE)).forEach(cookie -> {
+                response.addHeader(HttpHeaders.SET_COOKIE, cookie);
+            });
+
+            setAuthentication(authTokenResponse.accessToken());
+        }
         filterChain.doFilter(request, response);
     }
 
-    private String getToken(String token) {
-        return token.substring(7);
+    private void setAuthentication(String token) {
+        TokenDto tokenDto = jwtUtil.parseToken(token);
+
+        if (tokenDto != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = new PrincipalDetails(tokenDto.memberId(), tokenDto.memberRole());
+            Authentication authentication = UsernamePasswordAuthenticationToken.authenticated(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
     }
 
-    private boolean hasBearerToken(String token) {
-        return token != null && token.startsWith("Bearer ");
+
+    private String getAccessTokenByHeader(HttpServletRequest request) {
+        String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (accessToken != null && accessToken.startsWith("Bearer ")) {
+            return accessToken.substring(7);
+        }
+        return null;
+    }
+
+    private String getAccessTokenByCookie(HttpServletRequest request) {
+        Cookie accessTokenCookie = WebUtils.getCookie(request, ACCESS_TOKEN_COOKIE_NAME);
+        if (accessTokenCookie != null) {
+            return accessTokenCookie.getValue();
+        }
+        return null;
+    }
+
+    private String getRefreshTokenByCookie(HttpServletRequest request) {
+        Cookie refreshTokenCookie = WebUtils.getCookie(request, REFRESH_TOKEN_COOKIE_NAME);
+        if (refreshTokenCookie != null) {
+            return refreshTokenCookie.getValue();
+        }
+        return null;
     }
 }
