04b5c0c89f01b8718b2b8d539d56222fdebe79de
eunlee <eklee2057@naver.com>
2025-06-10 15:41:45 +0900

    feat: MemberServiceTest 작성

--- src/test/java/io/goorm/member/service/MemberServiceTest.java
+++ src/test/java/io/goorm/member/service/MemberServiceTest.java
@@ -4,7 +4,11 @@ import io.goorm.config.dto.PrincipalDetails;
 import io.goorm.config.exception.CustomException;
 import io.goorm.member.dao.MemberRepository;
 import io.goorm.member.domain.Member;
+import io.goorm.member.domain.MemberRole;
+import io.goorm.member.dto.request.MemberUpdateRequest;
 import io.goorm.member.dto.response.MemberFindMeResponse;
+import io.goorm.member.dto.response.MemberResponse;
+import io.goorm.member.dto.response.PageResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,9 +16,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.mockito.*;
@@ -22,6 +28,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -57,18 +64,15 @@ class MemberServiceTest {
         );
     }
 
-
     @Test
     void 내_프로필_조회() {
 
         //given
-
         long memberId = Long.parseLong(principalDetails.getUsername());
 
         //when
         when(memberRepository.findById(memberId)).thenReturn(Optional.of(member));
         MemberFindMeResponse response = memberService.findMember(principalDetails);
-
 
         //then
         Assertions.assertEquals(response.nickname(), member.getNickname());
@@ -76,44 +80,207 @@ class MemberServiceTest {
     }
 
     @Test
-    void 내_프로필_조회_실패_없는_아이디() {
+    void 내_프로필_조회_실패_존재하지_않는_아이디() {
+
         //given
         //when
-
         when(memberRepository.findById(member.getId())).thenReturn(Optional.empty());
 
         //then
         Assertions.assertThrows(CustomException.class, () -> memberService.findMember(principalDetails));
-
     }
 
     @Test
     void 전체_회원_조회() {
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        String searchValue = "test";
+        MemberRole role = MemberRole.USER;
+            List<Member> memberList = List.of(member);
+        Page<Member> memberPage = new PageImpl<>(memberList, pageable, memberList.size());
 
+        // when
+        when(memberRepository.findAllByPageableAndFilter(pageable, searchValue, role)).thenReturn(memberPage);
+        PageResponse<MemberResponse> response = memberService.findAll(pageable, searchValue, role);
+
+        // then
+        Assertions.assertEquals(1, response.content().size());
+        Assertions.assertEquals(member.getNickname(), response.content().get(0).nickname());
+        Assertions.assertEquals(member.getUsername(), response.content().get(0).username());
+    }
+
+    @Test
+    void 전체_회원_조회_searchValue가_없는_경우() {
+
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        String searchValue = null;
+        MemberRole role = MemberRole.USER;
+
+        List<Member> memberList = List.of(member);
+        Page<Member> memberPage = new PageImpl<>(memberList, pageable, memberList.size());
+
+        // when
+        when(memberRepository.findAllByPageableAndFilter(pageable, searchValue, role)).thenReturn(memberPage);
+        PageResponse<MemberResponse> response = memberService.findAll(pageable, searchValue, role);
+
+        // then
+        Assertions.assertEquals(1, response.content().size());
+    }
+
+    @Test
+    void 전체_회원_조회_searchValue의_결과가_존재하지_않는_경우() {
+
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        String searchValue = "XXXX";
+        MemberRole role = MemberRole.USER;
+
+        List<Member> memberList = List.of();
+        Page<Member> memberPage = new PageImpl<>(memberList, pageable, 0);
+
+        // when
+        when(memberRepository.findAllByPageableAndFilter(pageable, searchValue, role)).thenReturn(memberPage);
+        PageResponse<MemberResponse> response = memberService.findAll(pageable, searchValue, role);
+
+        // then
+        Assertions.assertEquals(0, response.content().size());
+    }
+
+    @Test
+    void 전체_회원_조회_role이_없는_경우() {
+
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        String searchValue = "test";
+        MemberRole role = null;
+
+        List<Member> memberList = List.of(member);
+        Page<Member> memberPage = new PageImpl<>(memberList, pageable, memberList.size());
+
+        // when
+        when(memberRepository.findAllByPageableAndFilter(pageable, searchValue, role)).thenReturn(memberPage);
+        PageResponse<MemberResponse> response = memberService.findAll(pageable, searchValue, role);
+
+        // then
+        Assertions.assertEquals(1, response.content().size());
+    }
+
+    @Test
+    void 전체_회원_조회_searchValue와_role이_없는_경우() {
+
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        String searchValue = null;
+        MemberRole role = null;
+
+        List<Member> memberList = List.of(member);
+        Page<Member> memberPage = new PageImpl<>(memberList, pageable, memberList.size());
+
+        // when
+        when(memberRepository.findAllByPageableAndFilter(pageable, searchValue, role)).thenReturn(memberPage);
+        PageResponse<MemberResponse> response = memberService.findAll(pageable, searchValue, role);
+
+        // then
+        Assertions.assertEquals(1, response.content().size());
+    }
+
+    @Test
+    void 전체_회원_조회_role이_ADMIN인_경우() {
+
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        String searchValue = "test";
+        MemberRole role = MemberRole.ADMIN;
+
+        ReflectionTestUtils.setField(member, "role", MemberRole.ADMIN);
+
+        List<Member> memberList = List.of(member);
+        Page<Member> memberPage = new PageImpl<>(memberList, pageable, memberList.size());
+
+        // when
+        when(memberRepository.findAllByPageableAndFilter(pageable, searchValue, role)).thenReturn(memberPage);
+        PageResponse<MemberResponse> response = memberService.findAll(pageable, searchValue, role);
+
+        // then
+        Assertions.assertEquals(1, response.content().size());
     }
 
     @Test
     void 내_정보_수정_닉네임만() {
 
+        //given
+        String newNickname = "newNickname";
+        MemberUpdateRequest request = new MemberUpdateRequest(newNickname, null, null);
+
+        //when
+        when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+
+        memberService.updateMember(principalDetails, request);
+
+        //then
+        Assertions.assertEquals(newNickname, member.getNickname());
     }
 
     @Test
     void 내_정보_수정_패스워드만() {
 
+        //given
+        String currentPassword = "testPassword";
+        String newPassword = "newTestPassword";
+        MemberUpdateRequest request = new MemberUpdateRequest(null, currentPassword, newPassword);
+
+        //when
+        when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+        when(passwordEncoder.matches(currentPassword, member.getPassword())).thenReturn(true);
+        when(passwordEncoder.encode(newPassword)).thenReturn("encodedNewPassword");
+
+//        System.out.println(member);
+        memberService.updateMember(principalDetails, request);
+
+        //then
+        Assertions.assertEquals("encodedNewPassword", member.getPassword());
     }
 
     @Test
-    void 내_정보_수정_닉네임_패스워드() {
+    void 내_정보_수정_닉네임_패스워드_모두() {
 
+        //given
+        String newNickname = "newNickname";
+        String currentPassword = "testPassword";
+        String newPassword = "newTestPassword";
+        MemberUpdateRequest request = new MemberUpdateRequest(newNickname, currentPassword, newPassword);
+
+        //when
+        when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+        when(passwordEncoder.matches(currentPassword, member.getPassword())).thenReturn(true);
+        when(passwordEncoder.encode(newPassword)).thenReturn("encodedNewPassword");
+        memberService.updateMember(principalDetails, request);
+
+        //then
+        Assertions.assertEquals(newNickname, member.getNickname());
+        Assertions.assertEquals("encodedNewPassword", member.getPassword());
+    }
+
+    @Test
+    void 내_정보_수정_실패_현재_비밀번호가_틀린_경우() {
+
+        //given
+        String newNickname = "newNickname";
+        String currentPassword = "wrongPassword";
+        String newPassword = "newTestPassword";
+        MemberUpdateRequest request = new MemberUpdateRequest(newNickname, currentPassword, newPassword);
+
+        //when
+        when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+        when(passwordEncoder.matches(currentPassword, member.getPassword())).thenReturn(false);
+
+        //then
+        Assertions.assertThrows(CustomException.class, () -> memberService.updateMember(principalDetails, request));
     }
 
     @Test
     void 탈퇴() {
-        //delete 로직 수행
-
-        //deletedAt != null && deletedAt == currentTime
-        // findById -> member가 리턴이 되어야하고,
-        // member.deleteMember(); 현재 날짜로 바꾸어야 하고;
 
         //given
         when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
@@ -125,7 +292,6 @@ class MemberServiceTest {
         LocalDateTime now = LocalDateTime.now();
 
         Duration duration = Duration.between(deletedAt, now);
-
 
         //then
         Assertions.assertNotNull(member.getDeletedAt());
